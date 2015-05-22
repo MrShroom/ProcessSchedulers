1e675b07b300db577a31ffb8d2fd1dade4b058f5
Shaun McThomas <shaunrm23@gmail.com>
2015-05-22 14:30:27 -0700

    Ready to move on. Process class needs to be tested
    add all methods needed. Ready to move on. will test later.

--- Process.java
+++ Process.java
@@ -1,10 +1,19 @@
+/***
+*	@author Mr.Shroom
+*	Created by: Shaun McThomas
+*	Last Modified:     05/22/15
+*	This a simple class to represent a process in a scheduler algorithm
+*	It will be used in simulations 
+****/
+
+import java.util.*;
 
 public class Process
 {
 	private Long processId; //the process id
-	private Integer arrivalTime; //the time the process arrived
-	private Integer lastTime; //the last time the process had the CPU
-	private Integer turnAroundTime;//the process turn around time
+	private int arrivalTime; //the time the process arrived
+	private int lastTime; //the last time the process had the CPU
+	private int turnAroundTime;//the process turn around time
 	private int burstTime; //the total burst time
 	private int share; //the share used only for PS scheduler
 	private int remainingBurstTime; //the remaining burst
@@ -14,34 +23,47 @@ public class Process
 	*constructor with needed parameters
 	*
 	**/
-	public Process(Long processId,Integer arrivalTime, int bustTime, int share)
+	public Process(Long processId,int arrivalTime, int burstTime, int share)
 	{
-		setAtributes(processId,arrivalTime, bustTime, share);
+		setAtributes(processId,arrivalTime, burstTime, share, burstTime, arrivalTime,0,0);
 	}
 
 	/**
 	*constructor with needed parameters
 	*
 	**/
-	public Process(Long processId,Integer arrivalTime, int bustTime)
+	public Process(Long processId,int arrivalTime, int burstTime)
 	{
-		setAtributes(processId,arrivalTime, bustTime, 0);
+		setAtributes(processId,arrivalTime, burstTime, 0, burstTime, arrivalTime,0,0);
 	}
 
+	/**
+	* copy constructor
+	*
+	**/
+	public Process(Process toCopy)
+	{
+		setAtributes(toCopy.processId,toCopy.arrivalTime, toCopy.burstTime, 
+								toCopy.share, toCopy.remainingBurstTime, toCopy.lastTime, 
+								toCopy.currentWaitTime, toCopy.turnAroundTime );
+	}
+	
 	/**
 	*setter for process Attributes
 	*
 	**/
-	private void setAtributes(Long processId, Integer arrivalTime, int bustTime, int share)
+	private void setAtributes(Long processId, int arrivalTime, int bustTime, 
+								int share, int remainingBurstTime, int lastTime, 
+								int currentWaitTime, int turnAroundTime )
 	{
-		this.processId =new Integer(processId);
-		this.arrivalTime = new Integer( arrivalTime);
+		this.processId =new Long(processId);
+		this.arrivalTime = arrivalTime;
 		this.burstTime = burstTime;
 		this.share = share;
-		this.remainingBurstTime = bustTime;
-		this.lastTime = new (arrivalTime);
-		this.currentWaitTime = 0;
-		this.turnAroundTime = 0;
+		this.remainingBurstTime = remainingBurstTime;
+		this.lastTime = lastTime;
+		this.currentWaitTime = currentWaitTime;
+		this.turnAroundTime = turnAroundTime;
 	}
 	/**
 	*getter
@@ -54,7 +76,7 @@ public class Process
 	/**
 	*getter
 	**/
-	public Integer getArrivalTime()
+	public int getArrivalTime()
 	{
 		return arrivalTime;
 	}
@@ -70,7 +92,7 @@ public class Process
 	/**
 	*getter
 	**/
-	public int getshare()
+	public int getShare()
 	{
 		return share;
 	}
@@ -86,25 +108,56 @@ public class Process
 	/**
 	*getter
 	**/
-	public int getcurrentWaitTime()
+	public int getCurrentWaitTime()
 	{
 		return currentWaitTime;
 	}
 	
+	/**
+	*check if process is finished running
+	**/
 	public boolean isFinished ()
 	{
 		return (remainingBurstTime <= 0);
 	}
 	
-	public void runFor (int sytemTime, int runAmount)
+	/**
+	*	This function handles the running a process for a set amount of time.
+	*	Where it finishes it returns what the new system time should be
+	*
+	*	@param systemTime	The system time before running the process
+	*	@param runAmount	The amount of time allocated for the process to run.
+	*
+	*	@return				What the new system time should be.
+	**/
+	public int runFor (int systemTime, int runAmount)
 	{
-		if(!isFinished())
+		if(!isFinished())								//if the process is finished, we don't run it
 		{
-			remainingBurstTime -= runAmount;
-			currentWaitTime += (sytemTime - lastTime);
-			lastTime = sytemTime + runAmount;
+			if(remainingBurstTime < runAmount)			//make sure we don't run past the end 
+				runAmount = remainingBurstTime;
+				
+			remainingBurstTime -= runAmount;			//logic involved in the running of a process
+			currentWaitTime += (systemTime - lastTime);	//bookkeeping so we can easily know wait time 
+			lastTime = systemTime + runAmount;			//needed to figure out wait and turn Around Time
+			
+			if(isFinished())
+				turnAroundTime = lastTime - arrivalTime; //now that we're done we can calculate turn Around Time
+			return lastTime;
 		}
+		return systemTime;
 		
+	}
+	
+	/**
+	*	This function will output process as a string in the format request by the assignment
+	*	<process-id> <finish-time> <wait-time> <turnaround-time>
+	*
+	*	@return			The requested string.
+	**/
+	public String output()
+	{
+		return(new String(processId + " " + lastTime + " " + currentWaitTime + " " + turnAroundTime));
 	}
 }
 	
